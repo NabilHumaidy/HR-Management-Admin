541459f7a144dc560d692d70dbf89c35ebadfb6d
NabilHumaidy <153157381+NabilHumaidy@users.noreply.github.com>
2023-12-29 10:38:13 +0700

    Update ApplicationLogo.jsx

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,5 @@
 export default function ApplicationLogo(props) {
     return (
-        <img src="img/LogoText.svg"  className="w-[180px]" alt="" />
+        <img src="image/svg/LogoText.svg"  className="w-[180px]" alt="" />
     );
 }
