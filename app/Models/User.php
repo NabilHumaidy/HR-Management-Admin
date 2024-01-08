16eb2b38ed89ca02099ec4e61d815adca38b6a48
NabilHumaidy <153157381+NabilHumaidy@users.noreply.github.com>
2024-01-08 14:39:24 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -43,3 +39,4 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 }
+employeuse
