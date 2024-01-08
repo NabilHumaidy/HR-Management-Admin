a0b8a9b63539cab24d4a8aea552bebaa443054a0
NabilHumaidy <153157381+NabilHumaidy@users.noreply.github.com>
2024-01-08 14:50:30 +0700

    Update AppDepartments.jsx

--- resources/js/Pages/Departments/AppDepartments.jsx
+++ resources/js/Pages/Departments/AppDepartments.jsx
@@ -9,6 +9,7 @@ export default function AppDepartments({auth, errors, departments, employee}) {
     let user = auth.user;
     const { props } = usePage();
     const flashMessage = props.flash.message;
+
     const [isAlert, setIsAlert] = useState(true);
 
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
@@ -27,7 +28,6 @@ export default function AppDepartments({auth, errors, departments, employee}) {
         setData(formData);
     
         router.post('/dashboard/departments/add', formData);
-        setDepartmentName('');
     }
 
     console.log('last props: ', props);
@@ -42,9 +42,9 @@ export default function AppDepartments({auth, errors, departments, employee}) {
             >
                 <div className='h-full border p-5 rounded-xl'>
                     <div className='flex flex-col md:flex-row justify-between'>
-                        <div className='flex rounded-lg border items-center mb-3 md:mb-0 gap-2'>
+                        <div className='flex rounded-lg border items-center mb-3 md:mb-0'>
                             <img src="/image/svg/search.svg" alt="" className='pl-[10px] h-6 ' />
-                            <input type="text" placeholder="Search" className='rounded-md opacity-40 border-transparent outline-0' />
+                            <input type="text" placeholder="Search" className='border-0 rounded-md opacity-40 border-transparent focus:border-transparent focus:ring-0' />
                         </div>
                         <div className='flex gap-5 justify-between md:justify-normal'>
                             <Button onPress={onOpen} className='gap-2 text-white bg-primary-500 p-2 md:p-3 rounded-xl' startContent={<HiOutlinePlusCircle size={25}/>}>Add New Department</Button>
@@ -66,29 +66,38 @@ export default function AppDepartments({auth, errors, departments, employee}) {
                                         </div>
                                         <div className='pt-5 flex flex-col gap-4'>
                                             {employee ? employee.map((karyawan, j) => {
-                                                return(
-                                                    <Link key={j} className='flex justify-between items-center'>
-                                                        <div className='flex gap-2 items-center'>
-                                                            <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-                                                            <div className='flex flex-col'>
-                                                                <p>{karyawan.name}</p>
-                                                                <p className='text-xs text-[#A2A1A8]'>CEO</p>
+                                                console.log(karyawan);
+                                                // Check apakah department.id sama dengan karyawan.department
+                                                if (department.id == karyawan.department) {
+                                                    return (
+                                                        <Link key={j} href={`/dashboard/employees/show/${karyawan.id}`} className='flex justify-between items-center'>
+                                                            <div className='flex gap-2 items-center'>
+                                                                <Avatar src={`/uploads/Photo Profile/${karyawan.photoProfile ? karyawan.photoProfile : 'defaultAvatar.JPG'}`} />
+                                                                <div className='flex flex-col'>
+                                                                    <p>
+                                                                        {`${karyawan.firstName} ${karyawan.lastName}`}
+                                                                    </p>
+                                                                    <p className='text-xs text-[#A2A1A8]'>
+                                                                        {karyawan.designation}
+                                                                    </p>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        <div>
-                                                            <FaAngleRight />
-                                                        </div>
-                                                    </Link>
-                                                );
+                                                            <div>
+                                                                <FaAngleRight />
+                                                            </div>
+                                                        </Link>
+                                                    );
+                                                } else {
+                                                    return null; // Jika tidak sesuai, kembalikan null
+                                                }
                                             }) : 'Tidak ada Data'}
-                                            
                                         </div>
                                     </div>
                                 )
                             })}
 
                         </div>
-                    <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={false}>
+                    <Modal backdrop={'blur'} isOpen={isOpen} onOpenChange={onOpenChange} size={'sm'} isDismissable={false}>
                         <ModalContent>
                         {(onClose) => (
                             <>
@@ -115,15 +124,16 @@ export default function AppDepartments({auth, errors, departments, employee}) {
                                 {
                                     errors ? <p className='text-sm text-red-500'>{errors.departmentName}</p> : ''
                                 }
+
+                                <div className="flex justify-center gap-[11px] p-5 w-full">
+                                    <Button className="border rounded-lg bg-transparent w-full" onPress={onClose}>
+                                        Close
+                                    </Button>
+                                    <Button className="bg-primary-500 border rounded-lg w-full text-white" onClick={() => handleSubmit()}>
+                                        Add
+                                    </Button>
+                                </div>
                             </ModalBody>
-                            <ModalFooter>
-                                <Button color="danger" variant="light" onPress={onClose}>
-                                    Close
-                                </Button>
-                                <Button className='bg-primary-500' onClick={() => handleSubmit()}>
-                                    Save
-                                </Button>
-                            </ModalFooter>
                             </>
                         )}
                         </ModalContent>
