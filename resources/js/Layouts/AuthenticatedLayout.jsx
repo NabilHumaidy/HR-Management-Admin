ccf74e8bf1abf45d53a2654cbbe42063e83a0032
NabilHumaidy <153157381+NabilHumaidy@users.noreply.github.com>
2024-01-08 14:48:54 +0700

    Update AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
@@ -8,15 +8,31 @@ import { SideBar } from '@/Components/SideBar';
 import Navbar from '@/Components/NavBar';
 import { HiBars3, HiXMark } from "react-icons/hi2";
 import DashboardMenu from '@/Components/DashboardMenu';
-import {NextUIProvider} from "@nextui-org/react";
 
 export default function Authenticated({ user, header, headerAlt,children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-    const [ openSideBar, setOpenSideBar ] = useState(true);
+    const [ openSideBar, setOpenSideBar ] = useState(false);
     const { bodyClass } = usePage();
+    useEffect(() => {
+        // Fungsi untuk menangani perubahan ukuran layar
+        const handleResize = () => {
+          // Sesuaikan nilai openSideBar sesuai dengan ukuran layar
+          setOpenSideBar(window.innerWidth >= 1024); // Misalnya, atur nilai 768 sebagai batas ukuran layar untuk mode desktop
+        };
+    
+        // Panggil handleResize saat komponen dimuat dan saat ukuran layar berubah
+        handleResize();
+        window.addEventListener('resize', handleResize);
+    
+        // Membersihkan event listener saat komponen dibongkar
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+      }, []); // useEffect hanya dipanggil sekali saat komponen dimuat
 
     // Menghapus kelas text-sans jika ada
     document.body.classList.remove('font-sans', 'antialiased');
+    
     return (
         <>
             <div className="flex h-screen flex-col md:flex-row">
